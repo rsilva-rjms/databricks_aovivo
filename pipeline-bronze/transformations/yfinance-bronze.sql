ea07afca856f07d5671c8687e48b0cea66be5be7
rsilva-rjms <rsilva-rjms>
2025-10-15 18:21:08 +0000

    lakeflow-config
    configuração dos agendamentos da camada bronze

---
+++ pipeline-bronze/transformations/yfinance-bronze.sql
@@ -0,0 +1,8 @@
+CREATE OR REFRESH STREAMING LIVE TABLE lakehouse.bronze.yfinance
+TBLPROPERTIES ("quality" = "bronze")
+AS
+SELECT *
+FROM cloud_files(
+  '/Volumes/lakehouse/raw_public/yfinance/commodities/latest_prices/',
+  'json'
+);
